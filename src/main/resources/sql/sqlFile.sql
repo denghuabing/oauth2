ec1fe7d42eeb1f8a111646c387ce512b1e276af5
denghuabing <denghuabing00468>
2019-10-01 23:15:56 +0800

    11

---
+++ src/main/resources/sql/sqlFile.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `user` (
+  `id` varchar(64) NOT NULL,
+  `userName` varchar(32) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
